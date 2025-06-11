4af91648b41221a814065d7a415460a087600e7f
Adrian Kiptoo <cherukip.adrian@gmail.com>
2025-06-11 14:46:47 +0300

    the last change

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,8 +20,38 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.get(id)
 
+    if earthquake:
+        return jsonify({
+            "id": earthquake.id,
+            "magnitude": earthquake.magnitude,
+            "location": earthquake.location,
+            "year": earthquake.year
+        })
+    else:
+        return jsonify({"message": "Earthquake 9999 not found."}), 404
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    response = {
+        "count": len(quakes),
+        "quakes": [
+            {
+                "id": quake.id,
+                "magnitude": quake.magnitude,
+                "location": quake.location,
+                "year": quake.year
+            }
+            for quake in quakes
+        ]
+    }
+
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
